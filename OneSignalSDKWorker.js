8effaccdfe9e471b467f5789fd5e9ad7063cebe4
Saban-94 <hsaban2025@gmail.com>
2025-09-29 14:06:55 +0300

    Add files via upload

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,5 @@
+/**
+ * This file must be placed in the root directory of your site.
+ * It is required for OneSignal push notifications to work.
+ */
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
